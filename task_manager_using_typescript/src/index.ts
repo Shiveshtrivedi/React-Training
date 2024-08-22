653f334e63654f8569979ade9f7437773f373cf2
shivesh trivedi <shivesh.trivedi@intimetec.com>
2024-08-22 10:33:24 +0530

    resolve enum error in github actions

--- task_manager_using_typescript/src/index.ts
+++ task_manager_using_typescript/src/index.ts
@@ -1,6 +1,6 @@
-import { IAllTasks, ITask } from './utils/interfaces';
+import { IAllTasks, ITask } from './utils/interfaces.js';
 import { fetchData, handleError } from './data_file.js';
-import { FilterTypeValue, DateFormat } from './utils/enum';
+import { FilterTypeValue, DateFormat } from './utils/enum.js';
 
 const addTask = document.querySelector('.addTaskSymbol') as HTMLDivElement;
 const addTaskPopupOverlay = document.querySelector(
